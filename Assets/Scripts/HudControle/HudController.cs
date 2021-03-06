7660ee24961da02205d9674b2515454b5f761cb0
Valdeir Antonio <valdeir.ans@gmail.com>
2021-03-06 10:48:23 +0100

    completed

---
+++ Assets/Scripts/HudControle/HudController.cs
@@ -0,0 +1,53 @@
+﻿/*******************************************************************************
+Copyright (c) 2021 INSIDE * All rights reserved. 
+Programador: Valdeir Antonio do Nascimento
+Data: 06/03/2021
+Projeto: Teste INSIDE
+*****************************************************************************/
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HudController : MonoBehaviour
+{
+    #region VARIAVEIS PRIVADAS
+
+    private Image _valorPotencia;
+
+    [SerializeField] private RectTransform _marcadorDePotencia;
+  
+    #endregion
+
+    #region MÉTODOS UNITY
+    void Awake()
+    {
+        _valorPotencia = _marcadorDePotencia.GetChild(0).GetComponent<Image>();
+    }	
+    void Start()
+    {
+        
+    }
+    private void OnEnable()
+    {
+       
+    } 
+    void Update()
+    {
+
+    }
+    private void OnDisable()
+    {
+        
+    }
+    #endregion
+
+    #region MÉTODOS PRÓPRIOS
+    public void ExibirPotencia(float potencia)
+    {
+        _valorPotencia.fillAmount = potencia;
+    }
+    #endregion
+}
--- Assets/Scripts/Jogador/Player.cs
+++ Assets/Scripts/Jogador/Player.cs
@@ -10,27 +10,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public delegate void MarcarPotencia(float potencia);
 public class Player : MonoBehaviour
 {
     #region VARIAVEIS PRIVADAS
+
+    private bool _arremesso;
 
     [SerializeField] private float _time;
 
     [SerializeField] private Bola _bola;
     #endregion
 
+    private event MarcarPotencia marcarPotencia;
+
     #region MÉTODOS UNITY
-     
+    private void Awake()
+    {
+        marcarPotencia += FindObjectOfType<HudController>().ExibirPotencia;
+    }
+
     void Update()
     {
-        _time = Mathf.PingPong(Time.time, 3)/3;
+        if (!_arremesso)
+        {   
+            _time = Mathf.PingPong(Time.time, 1); 
+        } 
+
+        marcarPotencia?.Invoke(_time);
     } 
     #endregion
 
     #region MÉTODOS PRÓPRIOS
     public void Arremessar()
     {
-        _bola.Arremessar(_time * 20, new Vector3(0.3f, 1, 0));
+        _bola.Arremessar(_time * 15, new Vector3(0.3f, 1, 0));
+
+        _arremesso = true;
     }
     #endregion
 }
