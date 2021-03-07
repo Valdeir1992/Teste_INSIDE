76e29ca848f6cdf2691dc5bdfb11b9f6051cebb7
Valdeir Antonio <valdeir.ans@gmail.com>
2021-03-07 20:29:14 +0100

    completed

--- Assets/Scripts/Jogador/Player.cs
+++ Assets/Scripts/Jogador/Player.cs
@@ -4,8 +4,10 @@ Programador: Valdeir Antonio do Nascimento
 Data: 05/03/2021
 Projeto: Teste INSIDE
 *****************************************************************************/
- 
+
+using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public delegate void MarcarPotencia(float potencia);
 public delegate void MarcarTempo(float tempo);
@@ -14,7 +16,7 @@ public class Player : MonoBehaviour
 {
     #region VARIAVEIS PRIVADAS
 
-    private bool _arremesso;
+    private bool _podeArremessar = true;
 
     private float _potencia;
 
@@ -24,7 +26,11 @@ public class Player : MonoBehaviour
 
     private Transform _tabela;
 
-    [SerializeField] private Bola _bola;
+    private Bola _bola;
+
+    [SerializeField] private Bola _prefabBola;
+
+    [SerializeField] private Transform _mao;
     #endregion
 
     private event MarcarPotencia marcarPotencia;
@@ -50,55 +56,90 @@ public class Player : MonoBehaviour
         marcarAngulo?.Invoke(_angulo);
 
         OlharParaTabela();
-    }
 
-    void Update()
-    {
-        _time += Time.deltaTime;
+        StartCoroutine(_pegarBola());
 
-        if (!_arremesso)
-        {   
-            _potencia = Mathf.PingPong(Time.time, 1); 
-        }
-
-        marcarTempo?.Invoke(_time);
-
-        marcarPotencia?.Invoke(_potencia); 
+        _bola = Instantiate(_prefabBola, _mao, false);
     } 
     #endregion
 
     #region MÉTODOS PRÓPRIOS
-    public void Arremessar()
+    public void Arremessar(InputAction.CallbackContext ctx)
     {
-        Vector3 vetor = new Vector3(Mathf.Cos(_angulo * Mathf.Deg2Rad), Mathf.Sin(_angulo * Mathf.Deg2Rad), 0);
+        if (_bola == null || !ctx.performed) return;
+
+        Vector3 vetor = new Vector3(Mathf.Cos(_angulo * Mathf.Deg2Rad), Mathf.Sin(_angulo * Mathf.Deg2Rad), 0); 
 
         _bola.RigidBody.isKinematic = false;
 
-        _bola.RigidBody.AddForce(transform.InverseTransformDirection(vetor).normalized * _potencia * 12, ForceMode.Impulse);
+        _bola.RigidBody.AddForce(new Vector3(transform.forward.x * vetor.x,vetor.y,transform.forward.z * vetor.x) * _potencia * 40, ForceMode.Impulse);
 
-        _arremesso = true;
+        _bola = null;
+
+        _podeArremessar = false;
+
+        _mao.GetChild(0).SetParent(null);
     }
-    public void AumentarAngulo()
+    public void AumentarAngulo(InputAction.CallbackContext ctx)
     {
+        if (!ctx.performed) return;
+
         if (_angulo >= 90) return;
+
         _angulo++;
+
         marcarAngulo?.Invoke(_angulo);
     }
-    public void DiminuirAngulo()
+    public void DiminuirAngulo(InputAction.CallbackContext ctx)
     {
+        if (!ctx.performed) return;
+
         if (_angulo <= 30) return; 
+
         _angulo--; 
+
         marcarAngulo?.Invoke(_angulo);
     }
 
     private void OlharParaTabela()
     {
 
-        Vector3 direction = (_tabela.position - transform.position).normalized;
+        Vector3 direction = (_tabela.position - transform.position);
 
         Quaternion lookToward = Quaternion.LookRotation(direction,transform.up);
 
         transform.rotation = Quaternion.Euler(0, lookToward.eulerAngles.y,0);
     }
     #endregion
+
+    private IEnumerator _pegarBola()
+    {
+        float timeElapsed = 0;
+
+        while (true)
+        {
+            while (!_podeArremessar)
+            {
+                timeElapsed += Time.deltaTime;
+                if (timeElapsed > 1)
+                {
+                    _podeArremessar = true;
+
+                    _bola = Instantiate(_prefabBola, _mao, false); 
+
+                    timeElapsed = 0;
+                }
+                yield return null; 
+            }
+            _potencia = Mathf.PingPong(Time.time, 1);
+
+            _time += Time.deltaTime;
+
+            marcarTempo?.Invoke(_time);
+
+            marcarPotencia?.Invoke(_potencia);
+
+            yield return null;
+        }
+    }
 }
